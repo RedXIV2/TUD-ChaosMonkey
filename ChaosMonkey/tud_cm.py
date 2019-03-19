13e757ead090fbad130d210155cdc2089eba6f37
Dave Hill <davidhill252@gmail.com>
2019-03-19 23:55:22 +0000

    Adding first wave of boto functionality, for Account retrieval and EC2 status

--- ChaosMonkey/tud_cm.py
+++ ChaosMonkey/tud_cm.py
@@ -3,13 +3,23 @@
 import sys
 import boto3
 
-print ("Welcome to the TUD Chaos Monkey!")
+print("Welcome to the TUD Chaos Monkey!")
+
+
+def accountDetails():
+    stsclient = boto3.client('sts')
+    myDetails = stsclient.get_caller_identity()
+    print("Account ID = ", myDetails["Account"])
+    print("User ID = ", myDetails["UserId"])
+
+
+accountDetails()
 
 num = 4
 
 instanceCountStr = "You current have {} instances running".format(num)
 
-print (instanceCountStr)
+print(instanceCountStr)
 
 monkeyShouldSmash = input("How many instances would you like to disrupt?")
 
@@ -22,13 +32,22 @@ updatedNum = 2
 
 updatedInstanceCountStr = "You now have {} instances running".format(updatedNum)
 
-print (updatedInstanceCountStr)
+print(updatedInstanceCountStr)
 
 
-print ("Now timing reinstatement")
-print {"Please wait while we test our recoverability"}
+print("Now timing reinstatement")
+print("Please wait while we test our recoverability")
 
-print (instanceCountStr)
+print(instanceCountStr)
 
-print ("====CHAOS MONKEY TEST RESULT====")
+print("====CHAOS MONKEY TEST RESULT====")
 
+def getEc2instances():
+    ec2client = boto3.resource('ec2')
+    instances = ec2client.instances.filter(
+    Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+    for instance in instances:
+        print(instance.id, instance.instance_type)
+
+
+getEc2instances()
